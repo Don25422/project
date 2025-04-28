f1d267f4403ad6cf3e734631657a3841e694539b
Don25422 <danglaskimathi5@gmail.com>
2025-04-28 16:29:34 +0300

    first commit

---
+++ app/src/main/java/com/don/hustletracker/navigation/AppNavHost.kt
@@ -0,0 +1,61 @@
+package com.don.hustletracker.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.don.hustletracker.data.UserDatabase
+import com.don.hustletracker.repository.UserRepository
+import com.don.hustletracker.ui.screens.about.AboutScreen
+import com.don.hustletracker.ui.screens.auth.LoginScreen
+import com.don.hustletracker.ui.screens.auth.RegisterScreen
+import com.don.hustletracker.ui.screens.home.HomeScreen
+import com.don.hustletracker.viewmodel.AuthViewModel
+
+@Composable
+fun AppNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = ROUT_LOGIN
+) {
+
+    val context = LocalContext.current
+
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+        composable(ROUT_HOME) {
+            HomeScreen(navController)
+        }
+        composable(ROUT_ABOUT) {
+            AboutScreen(navController)
+        }
+
+        //AUTHENTICATION
+        // Initialize Room Database and Repository for Authentication
+        val appDatabase = UserDatabase.getDatabase(context)
+        val authRepository = UserRepository(appDatabase.userDao())
+        val authViewModel: AuthViewModel = AuthViewModel(authRepository)
+        composable(ROUT_REGISTER) {
+            RegisterScreen(authViewModel, navController) {
+                navController.navigate(ROUT_LOGIN) {
+                    popUpTo(ROUT_REGISTER) { inclusive = true }
+                }
+            }
+        }
+
+        composable(ROUT_LOGIN) {
+            LoginScreen(authViewModel, navController) {
+                navController.navigate(ROUT_HOME) {
+                    popUpTo(ROUT_LOGIN) { inclusive = true }
+                }
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/don/hustletracker/navigation/Routes.kt
@@ -0,0 +1,10 @@
+package com.don.hustletracker.navigation
+
+const val ROUT_HOME = "home"
+const val ROUT_ABOUT = "about"
+const val ROUT_ITEM = "about"
+const val ROUT_DASHBOARD = "about"
+
+//Authentication
+const val ROUT_REGISTER = "Register"
+const val ROUT_LOGIN = "Login"
